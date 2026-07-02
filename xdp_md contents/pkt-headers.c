b620cc212c4783f6c54e4f50e22013f8f84ce934
bushra-kh <bushrakhaled.official@gmail.com>
2026-07-02 23:33:24 +0500

    added more pkt header fields

--- xdp_md contents/pkt-headers.c
+++ xdp_md contents/pkt-headers.c
@@ -42,19 +42,46 @@ int pkt_Hcontents(struct xdp_md *context)
     if((void*)(payload +4) > data_end) //atleast 4bytes if msg to read
         return XDP_PASS;
 
-
-    bpf_printk("MAC Src:%02x:%02x:%02x  MAC Dest:%02x:%02x:%02x",
+    //mac addresses
+    bpf_printk("MAC Src:%02x:%02x:%02x  MAC Dest:%02x:%02x:%02x\n",
         ether->h_source[0], ether->h_source[1], ether->h_source[2],
         ether->h_dest[0], ether->h_dest[1], ether->h_dest[2]);
 
+
+
+    //ip addresses and ports
     bpf_printk("Src IP:PORT=%pI4:%u -> Dest IP:PORT=%pI4:%u\n", 
         &ip->saddr, bpf_ntohs(tcp->source), 
         &ip->daddr, bpf_ntohs(tcp->dest));
+        // Convert sequence and total length to host byte order
+    __u32 seq_num = bpf_ntohl(tcp->seq);
+    __u16 total_packet_bytes = bpf_ntohs(ip->tot_len);
+    //ip pkt size, ttl, seq#
+    bpf_printk("TCP Seq Num: %u", seq_num);
+    bpf_printk("Packet Size: %u bytes | TTL: %u\n", total_packet_bytes, ip->ttl);
+    
+    __u16 ip_packet_id = bpf_ntohs(ip->id);//16-bit IP ID from network byte order to host byte order
+    __u8 ip_version = ip->version; //extract the ip ver 
 
+    bpf_printk("IP Version: IPv%u", ip_version);
+    bpf_printk("IP Packet ID (Fragment Tracker): %u\n", ip_packet_id);
+
+
+
+    //connection flags
+    if (tcp->syn) {
+        bpf_printk("Connection Status: [SYN]\n");
+    } else if (tcp->fin) {
+        bpf_printk("Connection Status: [FIN]\n");
+    } else {
+        bpf_printk("Connection Status: [DATA/ACK]\n");
+    }
+
+    //payload snips(many of which are encrypted tho)
     bpf_printk("DATA CHUNKS: %c%c%c%c\n", 
         payload[0], payload[1], payload[2], payload[3]);
-    // bpf_printk()
-    
+    bpf_printk("---------\n");
+
     
     return XDP_PASS;
 }
