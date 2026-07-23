cd7f725ec76c941c7cdfd13297aa17dc88c1aeea
bushra-kh <bushrakhaled.official@gmail.com>
2026-07-23 12:46:35 +0500

    omp example book

---
+++ os/threads/omp-book.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <omp.h>
+
+int main()
+{
+    //seq code
+    #pragma omp parallel 
+    {
+        printf("I am parallel region."); 
+    }
+    //seq code
+    return 0;
+}
+/*
+to compile: gcc -fopenmp omp-book.c -o omp
+to run: ./omp
+
+output: When OpenMP encounters the directive
+#pragma omp parallel
+it creates as many threads as there are processing cores in the system
+
+It gave the statement 12 times.
+*/
